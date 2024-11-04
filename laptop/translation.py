3b96747689f44b50d8f023de9249bbbe7b3417bc
Khadizhadeveloper <hadyousha6@gmail.com>
2024-11-04 15:54:17 +0600

    Names of fields in Kyrgyz, Russian, English

--- laptop/translation.py
+++ laptop/translation.py
@@ -1,6 +1,11 @@
-from modeltranslation.translator import register, TranslationOptions
+from modeltranslation.translator import TranslationOptions, translator
 from .models import Laptop
 
-@register(Laptop)
+
 class LaptopTranslationOptions(TranslationOptions):
-    fields = ('name', 'brand', 'model', 'description', 'color')
+    fields = ("name", "brand", "model", "price", "description", "image",
+              "color", "stock", "processor", "ram", "storage", "screen_size", "release_date",
+              "warranty_period", "rating", "is_active")
+
+translator.register(Laptop, LaptopTranslationOptions)
+
