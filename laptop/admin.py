ecdfe527b9ebf9aec07188b93e51bc65e8646033
Khadizhadeveloper <hadyousha6@gmail.com>
2024-11-08 00:10:54 +0600

    Filters were added

--- laptop/admin.py
+++ laptop/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 
 from laptop.models import Laptop
 
+@admin.register(Laptop)
+class LaptopAdmin(admin.ModelAdmin):
+    # Добавляем фильтры по полям
+    list_filter = ['brand', 'price']
 
+    # Добавляем нужные поля для отображения
+    list_display = ['brand', 'price', 'ram', 'screen_size']
+
+    # Настройка поиска по полям
+    search_fields = ['brand']
 # Register your models here.
-admin.site.register(Laptop)
+
+
