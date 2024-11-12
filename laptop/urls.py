443b72d66f9590f634eb132b0e38e87f7ca16e49
Khadizhadeveloper <hadyousha6@gmail.com>
2024-11-13 00:29:30 +0600

    Small fix

--- laptop/urls.py
+++ laptop/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import LaptopListCreate, LaptopDetailUpdateDelete
+from .views import LaptopListCreate, LaptopDetailUpdateDelete, OrderCreateView, OrderListView
 
 
 
 urlpatterns = [
     path('', LaptopListCreate.as_view(), name='laptop-list-create'),
     path('<int:pk>/', LaptopDetailUpdateDelete.as_view(), name='laptop-detail-update-delete'),
+    path('order/list/', OrderListView.as_view(), name='order-list'),
+    path('order/', OrderCreateView.as_view(), name='order-create'),
 ]
