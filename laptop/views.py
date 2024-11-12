c591ea1b811936b30a636684b143fd1b99fdbb4f
Khadizhadeveloper <hadyousha6@gmail.com>
2024-11-13 00:28:47 +0600

    Views for order creating and viewing

--- laptop/views.py
+++ laptop/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import render
 from rest_framework import generics
-from .models import Laptop
-from .serializers import LaptopSerializer
+from .models import Laptop, Order
+from .serializers import LaptopSerializer, OrderSerializer
 from .permissions import IsAdminUser
 
 class LaptopListCreate(generics.ListCreateAPIView):
@@ -17,7 +17,12 @@ class LaptopDetailUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAdminUser]
 
 
+class OrderCreateView(generics.CreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
 
 
-
-
+class OrderListView(generics.ListAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAdminUser]
