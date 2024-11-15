34359e5f0fef0f556ca6069d676044ace455ae0b
Khadizhadeveloper <hadyousha6@gmail.com>
2024-11-15 23:39:32 +0600

    Model Order was added to admin panel

--- laptop/admin.py
+++ laptop/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from laptop.models import Laptop
+from laptop.models import Laptop, Order
+
 
 @admin.register(Laptop)
 class LaptopAdmin(admin.ModelAdmin):
@@ -13,5 +14,10 @@ class LaptopAdmin(admin.ModelAdmin):
     # Настройка поиска по полям
     search_fields = ['brand']
 # Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['name','phone','email', 'laptop', 'created_at']
+    search_fields = ['name', 'laptop']
+    list_filter = ['created_at', 'laptop']
 
 
