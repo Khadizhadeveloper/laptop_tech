d41ee802a49a440562e1876cda73fea114ac1b5d
Khadizhadeveloper <hadyousha6@gmail.com>
2024-11-15 23:38:42 +0600

    Some fields were removed from translation

--- laptop/translation.py
+++ laptop/translation.py
@@ -3,9 +3,7 @@ from .models import Laptop
 
 
 class LaptopTranslationOptions(TranslationOptions):
-    fields = ("name", "brand", "model", "price", "description", "image",
-              "color", "stock", "processor", "ram", "storage", "screen_size", "release_date",
-              "warranty_period", "rating", "is_active")
+    fields = ( "description","color",)
 
 translator.register(Laptop, LaptopTranslationOptions)
 
